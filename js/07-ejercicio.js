d8d1ad7a880edb02ea31ae474002605896d00ef4
CosmeNicolas <nicomas7590@gmail.com>
2023-11-09 02:51:12 -0300

    realice el ejercicio 3 hasta generar el tiro de dados aleatorio y el armado de la mitad de tabla del ejercicio

--- js/07-ejercicio.js
+++ js/07-ejercicio.js
@@ -6,9 +6,11 @@ const numero = parseInt(prompt('Ingrese un numero'))
 
 function multiplicar(numero){
   const multiplicadores = [1,2,3,4,5,6,7,8,9,10];
+  document.write(`<h2>➗✖️➖➕Tabla de Multiplicar ➕➖✖️➗</h2>`)
   for(let i = 0 ;i < multiplicadores.length; i++){
     resultado =  numero * multiplicadores[i]
-    console.log(resultado)
+    document.write(`La tabla del multiplicar del ${numero} es ${numero}  x ${multiplicadores[i]}  = ${resultado}`)
+    document.write(`<br>`)
   }
 }
 multiplicar(numero)
